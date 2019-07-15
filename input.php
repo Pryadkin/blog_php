4ff0c01aea8cf4bf858c86dd40c6ecc04127c03a
pryadkin.a <pryadkin.a@citilink.ru>
2019-07-15 18:31:44 +0300

    change index.php

--- input.php
+++ input.php
@@ -1,4 +1,9 @@
 <?php
+    require_once 'include/database.php'; // подключаем базу данных
+    require_once 'include/function.php'; // подключаем функции для связи с MySQL
+?>
+
+<?php
 
 // Функции ниже выводят все возможные ошибки
 ini_set('error_reporting', E_ALL);
